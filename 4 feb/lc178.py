bbcfa043bfc195746f4cb131cad69b390c5e738f
shri hari <thisismeshrihari@gmail.com>
2026-02-04 11:13:44 +0530

    stack question

---
+++ 4 feb/lc178.py
@@ -0,0 +1,20 @@
+from typing import *
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+        for i in tokens:
+            if i in '+-*/':
+                a = st.pop()
+                b = st.pop()
+                if i == '+':
+                    st.append(a+b)
+                elif i == '-':
+                    st.append(b-a)
+                elif i == '*':
+                    st.append(b*a)
+                else:
+                    st.append(int(b/a))
+            else:
+                st.append(int(i))
+        return st[0]
