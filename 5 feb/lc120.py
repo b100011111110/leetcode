5765e6a524eaf8da39b599d8fc6754d2b6f3267f
shri hari <thisismeshrihari@gmail.com>
2026-02-05 08:50:46 +0530

    made fix for dp non recursive

--- 5 feb/lc120.py
+++ 5 feb/lc120.py
@@ -3,15 +3,8 @@ from typing import *
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        mat = [[-1]*(i+1) for i in range(n)]
-        def traverse(i,j):
-            if i == n:
-                return 0
-            if mat[i][j] != -1:
-                return mat[i][j]
-            x = traverse(i+1,j)
-            y = traverse(i+1,j+1)
-            mat[i][j] = min(x,y)+triangle[i][j]
-            return min(x,y)+triangle[i][j]
-        x = traverse(0,0)
-        return x
+        dp = triangle[-1][:]  
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
+        return dp[0]
