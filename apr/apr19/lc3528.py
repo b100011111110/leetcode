c7af802a86900c244632199dfd12b8d59a63d372
Shri Hari <thisismeshrihari@gmail.com>
2026-04-19 23:47:35 +0530

    questions solve d on 19 march

---
+++ apr/apr19/lc3528.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def baseUnitConversions(self, conversions: List[List[int]]) -> List[int]:
+        ans = [1]*(len(conversions)+1)
+        for start,end,cost in conversions:
+            ans[end] =  (ans[start]*cost)%(10**9+7)
+        return ans
